32a32785d0822a6fc94a4bceeea6ab0930a1e437
Hongseob Lee <sjun2918@naver.com>
2023-08-30 17:29:17 +0900

    init: 프로젝트 초기 셋팅

---
+++ src/main/java/com/foo/gosucatcher/GosuCatcherApplication.java
@@ -0,0 +1,13 @@
+package com.foo.gosucatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GosuCatcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GosuCatcherApplication.class, args);
+    }
+
+}
