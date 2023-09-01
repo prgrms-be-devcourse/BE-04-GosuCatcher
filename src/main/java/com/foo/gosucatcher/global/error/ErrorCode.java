2adc2b2f29182bdbd51c8727f46dbab8888e0a62
Hongseob Lee <sjun2918@naver.com>
2023-09-01 14:37:25 +0900

    feat: 예외 핸들러 내용 추가/수정 (GS-32)

--- src/main/java/com/foo/gosucatcher/global/error/ErrorCode.java
+++ src/main/java/com/foo/gosucatcher/global/error/ErrorCode.java
@@ -8,7 +8,10 @@ import lombok.RequiredArgsConstructor;
 public enum ErrorCode {
 
 	//서버
-	INTERNAL_SERVER_ERROR("SERVER_001", "예기치 못한 오류가 발생했습니다.");
+	INTERNAL_SERVER_ERROR("S001", "예기치 못한 오류가 발생했습니다."),
+
+	//공용
+	INVALID_INPUT_VALUE("C001", "잘못된 값을 입력하셨습니다.");
 
 	private final String code;
 	private final String message;
--- src/main/java/com/foo/gosucatcher/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/foo/gosucatcher/global/error/GlobalExceptionHandler.java
@@ -1,8 +1,43 @@
 package com.foo.gosucatcher.global.error;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.foo.gosucatcher.global.error.exception.BusinessException;
+import com.foo.gosucatcher.global.error.exception.EntityNotFoundException;
+import com.foo.gosucatcher.global.error.exception.InvalidValueException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+	@ExceptionHandler(EntityNotFoundException.class)
+	public ResponseEntity<ErrorResponse> entityNotFoundException(EntityNotFoundException e) {
+		ErrorResponse errorResponse = ErrorResponse.of(e.getErrorCode());
+
+		return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+	}
+
+	@ExceptionHandler(InvalidValueException.class)
+	public ResponseEntity<ErrorResponse> invalidValueException(InvalidValueException e) {
+		ErrorResponse errorResponse = ErrorResponse.of(e.getErrorCode());
+
+		return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(BusinessException.class)
+	public ResponseEntity<ErrorResponse> businessException(BusinessException e) {
+		ErrorResponse errorResponse = ErrorResponse.of(e.getErrorCode());
+
+		return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ErrorResponse> methodArgumentNotValidException(MethodArgumentNotValidException e) {
+		ErrorResponse errorResponse = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR, e.getBindingResult());
+
+		return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+	}
 }
