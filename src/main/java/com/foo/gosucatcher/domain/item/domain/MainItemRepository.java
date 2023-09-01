6574875bc9f28a264993ccc74a104a372e9536d9
Hongseob Lee <sjun2918@naver.com>
2023-09-01 21:25:09 +0900

    feat: Main,Sub 아이템 Repository 생성 (GS-38)

---
+++ src/main/java/com/foo/gosucatcher/domain/item/domain/MainItemRepository.java
@@ -0,0 +1,6 @@
+package com.foo.gosucatcher.domain.item.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MainItemRepository extends JpaRepository<MainItem, Long> {
+}
