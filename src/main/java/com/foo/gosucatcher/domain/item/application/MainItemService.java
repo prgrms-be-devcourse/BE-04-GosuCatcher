076fb4c40ceccbffcebccd506e370c3c8d4319c2
Hongseob Lee <sjun2918@naver.com>
2023-09-02 13:18:38 +0900

    feat: MainItem 서비스 CRUD 추가 (GS-38)

---
+++ src/main/java/com/foo/gosucatcher/domain/item/application/MainItemService.java
@@ -0,0 +1,70 @@
+package com.foo.gosucatcher.domain.item.application;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.foo.gosucatcher.domain.item.application.dto.request.MainItemCreateRequest;
+import com.foo.gosucatcher.domain.item.application.dto.request.MainItemUpdateRequest;
+import com.foo.gosucatcher.domain.item.application.dto.response.MainItemResponse;
+import com.foo.gosucatcher.domain.item.application.dto.response.MainItemsResponse;
+import com.foo.gosucatcher.domain.item.domain.MainItem;
+import com.foo.gosucatcher.domain.item.domain.MainItemRepository;
+import com.foo.gosucatcher.global.error.ErrorCode;
+import com.foo.gosucatcher.global.error.exception.BusinessException;
+import com.foo.gosucatcher.global.error.exception.EntityNotFoundException;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class MainItemService {
+
+	private final MainItemRepository mainItemRepository;
+
+	public MainItemResponse create(MainItemCreateRequest request) {
+		duplicatedNameCheck(request.name());
+
+		MainItem mainItem = MainItemCreateRequest.toMainItem(request);
+
+		MainItem savedMainItem = mainItemRepository.save(mainItem);
+
+		return MainItemResponse.from(savedMainItem);
+	}
+
+	@Transactional(readOnly = true)
+	public MainItemResponse findById(Long id) {
+
+		MainItem mainItem = mainItemRepository.findById(id)
+			.orElseThrow(() -> new EntityNotFoundException(ErrorCode.INVALID_INPUT_VALUE));
+
+		return MainItemResponse.from(mainItem);
+	}
+
+	@Transactional(readOnly = true)
+	public MainItemsResponse findAll() {
+		List<MainItem> mainItemList = mainItemRepository.findAll();
+
+		return MainItemsResponse.from(mainItemList);
+	}
+
+	public Long update(MainItemUpdateRequest request) {
+		MainItem foundMainItem = mainItemRepository.findById(request.id())
+			.orElseThrow(() -> new EntityNotFoundException(ErrorCode.NOT_FOUND_MAIN_ITEM));
+
+		duplicatedNameCheck(request.name());
+
+		MainItem mainItem = MainItemUpdateRequest.toMainItem(request);
+		foundMainItem.update(mainItem);
+
+		return foundMainItem.getId();
+	}
+
+	private void duplicatedNameCheck(String name) {
+		mainItemRepository.findByName(name).ifPresent(mainItem -> {
+			throw new BusinessException(ErrorCode.DUPLICATED_MAIN_ITEM_NAME);
+		});
+	}
+}
