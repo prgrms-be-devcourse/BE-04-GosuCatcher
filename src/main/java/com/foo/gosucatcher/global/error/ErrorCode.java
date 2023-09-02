24b12954b1de4dc8e098ad8bfa059dfd715b15d5
Hongseob Lee <sjun2918@naver.com>
2023-09-02 14:16:24 +0900

    hotfix: 전역 핸들러 수정, 누락된 에러 코드 추가

--- src/main/java/com/foo/gosucatcher/global/error/ErrorCode.java
+++ src/main/java/com/foo/gosucatcher/global/error/ErrorCode.java
@@ -11,7 +11,10 @@ public enum ErrorCode {
 	INTERNAL_SERVER_ERROR("S001", "예기치 못한 오류가 발생했습니다."),
 
 	//공용
-	INVALID_INPUT_VALUE("C001", "잘못된 값을 입력하셨습니다.");
+	INVALID_INPUT_VALUE("C001", "잘못된 값을 입력하셨습니다."),
+
+	//고수
+	NOT_FOUND_EXPERT("E001", "존재하지 않는 고수입니다.");
 
 	private final String code;
 	private final String message;
