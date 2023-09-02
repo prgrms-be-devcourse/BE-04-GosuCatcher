344c6798dc6d693eb612afd6505257d101764675
Hongseob Lee <sjun2918@naver.com>
2023-09-02 14:59:29 +0900

    style: 잘못된 패키지 구조 변경 (GS-38)

--- src/main/java/com/foo/gosucatcher/domain/review/domain/ReviewRepository.java
+++ src/main/java/com/foo/gosucatcher/domain/review/domain/ReviewRepository.java
@@ -1,8 +1,6 @@
 package com.foo.gosucatcher.domain.review.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface ReviewRepository extends JpaRepository<Long, Review> {
+public interface ReviewRepository extends JpaRepository<Review, Long> {
 }
