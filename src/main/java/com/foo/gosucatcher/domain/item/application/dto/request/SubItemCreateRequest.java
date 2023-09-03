70e58df6f972b1c9ee48eae43e4894944d76538e
Hongseob Lee <sjun2918@naver.com>
2023-09-03 17:19:36 +0900

    feat: 하위 서비스 컨트롤러 구현 (GS-38)

--- src/main/java/com/foo/gosucatcher/domain/item/application/dto/request/SubItemCreateRequest.java
+++ src/main/java/com/foo/gosucatcher/domain/item/application/dto/request/SubItemCreateRequest.java
@@ -1,13 +1,14 @@
 package com.foo.gosucatcher.domain.item.application.dto.request;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import com.foo.gosucatcher.domain.item.domain.MainItem;
 import com.foo.gosucatcher.domain.item.domain.SubItem;
 
 public record SubItemCreateRequest(
-	@NotBlank
+	@NotNull
 	Long mainItemId,
 	@NotBlank(message = "하위 서비스명은 필수 입력 입니다.")
 	@Pattern(regexp = "^[가-힣0-9\\s]+$", message = "하위 서비스명은 한글과 숫자만 입력 가능합니다.")
---
+++ src/main/java/com/foo/gosucatcher/domain/item/presentation/SubItemController.java
@@ -0,0 +1,66 @@
+package com.foo.gosucatcher.domain.item.presentation;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.foo.gosucatcher.domain.item.application.SubItemService;
+import com.foo.gosucatcher.domain.item.application.dto.request.SubItemCreateRequest;
+import com.foo.gosucatcher.domain.item.application.dto.request.SubItemUpdateRequest;
+import com.foo.gosucatcher.domain.item.application.dto.response.SubItemResponse;
+import com.foo.gosucatcher.domain.item.application.dto.response.SubItemsResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/sub-items")
+public class SubItemController {
+
+	private final SubItemService subItemService;
+
+	@PostMapping
+	public ResponseEntity<SubItemResponse> create(@Validated @RequestBody SubItemCreateRequest request) {
+
+		SubItemResponse subItemResponse = subItemService.create(request);
+
+		return ResponseEntity.ok(subItemResponse);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<SubItemResponse> findOne(@PathVariable Long id) {
+		SubItemResponse subItemResponse = subItemService.findById(id);
+
+		return ResponseEntity.ok(subItemResponse);
+	}
+
+	@GetMapping
+	public ResponseEntity<SubItemsResponse> findAll() {
+		SubItemsResponse subItemsResponse = subItemService.findAll();
+
+		return ResponseEntity.ok(subItemsResponse);
+	}
+
+	@PatchMapping("{id}")
+	public ResponseEntity<Long> update(@PathVariable Long id,
+		@Validated @RequestBody SubItemUpdateRequest subItemUpdateRequest) {
+
+		Long subItemId = subItemService.update(id, subItemUpdateRequest);
+
+		return ResponseEntity.ok(subItemId);
+	}
+
+	@DeleteMapping("{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		subItemService.delete(id);
+
+		return ResponseEntity.ok(null);
+	}
+}
