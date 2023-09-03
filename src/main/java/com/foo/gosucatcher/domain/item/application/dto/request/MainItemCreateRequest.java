b55eb3fefd08045061bd69eb7250c6ba9443348a
Hongseob Lee <sjun2918@naver.com>
2023-09-03 21:10:45 +0900

    feat: DTO 필드들 길이 유효성 추가 (GS-38)

--- src/main/java/com/foo/gosucatcher/domain/item/application/dto/request/MainItemCreateRequest.java
+++ src/main/java/com/foo/gosucatcher/domain/item/application/dto/request/MainItemCreateRequest.java
@@ -2,6 +2,7 @@ package com.foo.gosucatcher.domain.item.application.dto.request;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import com.foo.gosucatcher.domain.item.domain.MainItem;
 
@@ -10,6 +11,7 @@ public record MainItemCreateRequest(
 	@Pattern(regexp = "^[가-힣0-9\\s]+$", message = "서비스명은 한글과 숫자만 입력 가능합니다.")
 	String name,
 	@NotBlank(message = "해당 서비스의 부가 설명을 적어주세요.")
+	@Size(min = 6, message = "부가 설명은 6자 이상 작성해 주세요.")
 	String description
 ) {
 
--- src/main/java/com/foo/gosucatcher/domain/item/application/dto/request/SubItemUpdateRequest.java
+++ src/main/java/com/foo/gosucatcher/domain/item/application/dto/request/SubItemUpdateRequest.java
@@ -2,6 +2,7 @@ package com.foo.gosucatcher.domain.item.application.dto.request;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import com.foo.gosucatcher.domain.item.domain.MainItem;
 import com.foo.gosucatcher.domain.item.domain.SubItem;
@@ -10,6 +11,7 @@ public record SubItemUpdateRequest(
 	@NotBlank(message = "하위 서비스명은 필수 입력 입니다.")
 	@Pattern(regexp = "^[가-힣0-9\\s]+$", message = "하위 서비스명은 한글과 숫자만 입력 가능합니다.")
 	String name,
+	@Size(min = 6, message = "부가 설명은 6자 이상 작성해 주세요.")
 	String description
 ) {
 
