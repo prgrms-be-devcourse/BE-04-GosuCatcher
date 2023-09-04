efd4a0c04d6ff4e8971ddb7ec90b0853acf745a2
Seungwon Han <49016275+SW-H@users.noreply.github.com>
2023-09-04 16:50:22 +0900

    test: 불필요한 어노테이션 삭제

--- src/test/java/com/foo/gosucatcher/domain/review/presentation/ControllerTest.java
+++ src/test/java/com/foo/gosucatcher/domain/review/presentation/ControllerTest.java
@@ -28,7 +28,6 @@ import com.foo.gosucatcher.global.error.ErrorCode;
 import com.foo.gosucatcher.global.error.exception.EntityNotFoundException;
 
 @WebMvcTest(ReviewController.class)
-@Rollback(value = false)
 class ControllerTest {
 	String apiBaseUrl = "/api/v1/reviews";
 
