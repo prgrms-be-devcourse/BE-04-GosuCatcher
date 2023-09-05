50b576a8296b8b15c6c6c6d2146435e26755aaed
kimihiqq <kimihiqq@gmail.com>
2023-09-05 15:32:25 +0900

    refactor: Expert 서비스 내 접근제한자 수정
    - duplicatedNameCheck의 접근제한자를 public에서 private으로 변경했습니다.

--- src/main/java/com/foo/gosucatcher/domain/expert/application/ExpertService.java
+++ src/main/java/com/foo/gosucatcher/domain/expert/application/ExpertService.java
@@ -77,7 +77,7 @@ public class ExpertService {
 		expertRepository.delete(expert);
 	}
 
-	public void duplicatedNameCheck(String storeName) {
+	private void duplicatedNameCheck(String storeName) {
 		Optional<Expert> existingExpert = expertRepository.findByStoreName(storeName);
 		if (existingExpert.isPresent()) {
 			throw new BusinessException(ErrorCode.DUPLICATED_EXPERT_STORENAME);
