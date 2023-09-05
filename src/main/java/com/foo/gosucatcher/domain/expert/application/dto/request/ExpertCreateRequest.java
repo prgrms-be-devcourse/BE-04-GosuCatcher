16ad3966d59a336d22356074060e0991df59cbb1
kimihiqq <kimihiqq@gmail.com>
2023-09-05 14:44:09 +0900

    refactor: Expert 요청 DTO 애너테이션 수정
    - @Max 추가
    - maxTravelDistance 타입 Integer로 변경

--- src/main/java/com/foo/gosucatcher/domain/expert/application/dto/request/ExpertCreateRequest.java
+++ src/main/java/com/foo/gosucatcher/domain/expert/application/dto/request/ExpertCreateRequest.java
@@ -1,5 +1,6 @@
 package com.foo.gosucatcher.domain.expert.application.dto.request;
 
+import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
@@ -12,14 +13,19 @@ public record ExpertCreateRequest(
 	@NotBlank(message = "업체명은 필수 입력입니다.")
 	@Size(max = 20, message = "업체명은 20자 미만이어야 합니다.")
 	String storeName,
+
 	@NotBlank(message = "위치는 필수 입력입니다.")
 	String location,
+
 	@NotNull(message = "거리는 필수 입력입니다.")
 	@Positive(message = "거리는 양수만 가능합니다.")
-	int maxTravelDistance,
+	@Max(value = 999, message = "거리는 최대 999km까지 가능합니다.")
+	Integer maxTravelDistance,
+
 	@NotBlank(message = "부가 설명을 적어주세요.")
 	String description
 ) {
+
 	public static Expert toExpert(Member member, ExpertCreateRequest request) {
 		return Expert.builder()
 			.member(member)
--- src/main/java/com/foo/gosucatcher/domain/expert/application/dto/request/ExpertUpdateRequest.java
+++ src/main/java/com/foo/gosucatcher/domain/expert/application/dto/request/ExpertUpdateRequest.java
@@ -1,5 +1,6 @@
 package com.foo.gosucatcher.domain.expert.application.dto.request;
 
+import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
@@ -11,14 +12,19 @@ public record ExpertUpdateRequest(
 	@NotBlank(message = "업체명은 필수 입력입니다.")
 	@Size(max = 20, message = "업체명은 20자 미만이어야 합니다.")
 	String storeName,
+
 	@NotBlank(message = "위치는 필수 입력입니다.")
 	String location,
+
 	@NotNull(message = "거리는 필수 입력입니다.")
 	@Positive(message = "거리는 양수만 가능합니다.")
-	int maxTravelDistance,
+	@Max(value = 999, message = "거리는 최대 999km까지 가능합니다.")
+	Integer maxTravelDistance,
+
 	@NotBlank(message = "부가 설명을 적어주세요.")
 	String description
 ) {
+
 	public static Expert toExpert(ExpertUpdateRequest request) {
 		return Expert.builder()
 			.storeName(request.storeName())
@@ -27,5 +33,4 @@ public record ExpertUpdateRequest(
 			.description(request.description())
 			.build();
 	}
-
 }
