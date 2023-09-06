7e66b7ff3206b6bdf4e6e72577a3f3eb7e632ea5
SW-H <hsw08@naver.com>
2023-09-07 00:12:24 +0900

    refactor

--- src/main/java/com/foo/gosucatcher/domain/review/application/dto/response/ReviewResponse.java
+++ src/main/java/com/foo/gosucatcher/domain/review/application/dto/response/ReviewResponse.java
@@ -5,19 +5,19 @@ import java.time.LocalDateTime;
 import com.foo.gosucatcher.domain.review.domain.Review;
 
 public record ReviewResponse(
-		Long id,
-		Long expertId,
-		Long writerId,
-		Long subItemId,
-		String description,
-		int rating,
-		LocalDateTime createdAt,
-		LocalDateTime updatedAt
+	Long id,
+	Long expertId,
+	Long writerId,
+	Long subItemId,
+	String description,
+	int rating,
+	LocalDateTime createdAt,
+	LocalDateTime updatedAt
 ) {
 
 	public static ReviewResponse from(Review review) {
 		return new ReviewResponse(review.getId(), review.getExpert().getId(), review.getMember().getId(),
-				review.getSubItem().getId(), review.getDescription(), review.getRating(), review.getCreatedAt(),
-				review.getUpdatedAt());
+			review.getSubItem().getId(), review.getDescription(), review.getRating(), review.getCreatedAt(),
+			review.getUpdatedAt());
 	}
 }
