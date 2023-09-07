9df10aaa0870c4da18b7917289521ca46ac40e1b
kimihiqq <kimihiqq@gmail.com>
2023-09-07 23:25:08 +0900

    feat: Image 추상클래스 생성

---
+++ src/main/java/com/foo/gosucatcher/domain/image/domain/Image.java
@@ -0,0 +1,20 @@
+package com.foo.gosucatcher.domain.image.domain;
+
+import javax.persistence.MappedSuperclass;
+
+import lombok.Getter;
+
+@MappedSuperclass
+@Getter
+public abstract class Image {
+	private String filename;
+	private String url;
+	private Long size;
+
+	public Image(String filename, String url, Long size) {
+		this.filename = filename;
+		this.url = url;
+		this.size = size;
+	}
+	public Image() {}
+}
