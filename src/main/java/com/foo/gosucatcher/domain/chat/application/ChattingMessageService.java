505ea70cf3eed449e0fba412b93d1db1100b36a1
hyee0715 <hyee0715@naver.com>
2023-09-10 18:58:17 +0900

    refactor: ChattingMessageService의 sendExpertEstimateMessage 메서드 수정 (GS-65)

--- src/main/java/com/foo/gosucatcher/domain/chat/application/ChattingMessageService.java
+++ src/main/java/com/foo/gosucatcher/domain/chat/application/ChattingMessageService.java
@@ -11,15 +11,15 @@ import com.foo.gosucatcher.domain.chat.domain.ChattingRoomRepository;
 import com.foo.gosucatcher.domain.estimate.application.dto.response.ExpertAutoEstimateResponse;
 import com.foo.gosucatcher.domain.member.domain.Member;
 import com.foo.gosucatcher.domain.member.domain.MemberRepository;
-import com.foo.gosucatcher.global.error.ErrorCode;
 import com.foo.gosucatcher.global.error.exception.BusinessException;
 import com.foo.gosucatcher.global.error.exception.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static com.foo.gosucatcher.global.error.ErrorCode.*;
 
@@ -67,14 +67,9 @@ public class ChattingMessageService {
             throw new BusinessException(CHATTING_ROOM_ASSIGNMENT_FAILED);
         }
 
-        List<ChattingMessageResponse> chattingMessageResponses = new ArrayList<>();
-
-        int chattingRoomCount = chattingRoomResponses.size();
-
-        for (int count = 0; count < chattingRoomCount; count++) {
-            ChattingMessageResponse chattingMessageResponse = create(expertAutoEstimateResponses.get(count).expert().id(), chattingRoomResponses.get(count).id(), expertAutoEstimateResponses.get(count).description());
-            chattingMessageResponses.add(chattingMessageResponse);
-        }
+        List<ChattingMessageResponse> chattingMessageResponses = IntStream.range(0, chattingRoomResponses.size())
+                .mapToObj(count -> create(expertAutoEstimateResponses.get(count).expert().id(), chattingRoomResponses.get(count).id(), expertAutoEstimateResponses.get(count).description()))
+                .collect(Collectors.toList());
 
         return ChattingMessagesResponse.valueOf(chattingMessageResponses);
     }
