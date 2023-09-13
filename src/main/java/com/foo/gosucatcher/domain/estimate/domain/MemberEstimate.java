686f426e4fbd5f59bdd0eb323414d3461dc5bcb2
hyee0715 <hyee0715@naver.com>
2023-09-13 15:18:32 +0900

    refactor: MemberEstimate의 @SQLDelete 수정 (GS-58)

--- src/main/java/com/foo/gosucatcher/domain/estimate/domain/MemberEstimate.java
+++ src/main/java/com/foo/gosucatcher/domain/estimate/domain/MemberEstimate.java
@@ -38,7 +38,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @Entity
 @Where(clause = "is_closed = false")
-@SQLDelete(sql = "UPDATE member_estimates SET is_closed = true, status = 'FINISH' WHERE id = ?")
+@SQLDelete(sql = "UPDATE member_estimates SET is_closed = true, status = 'FINISHED' WHERE id = ?")
 @Table(name = "member_estimates")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MemberEstimate extends BaseEntity {
