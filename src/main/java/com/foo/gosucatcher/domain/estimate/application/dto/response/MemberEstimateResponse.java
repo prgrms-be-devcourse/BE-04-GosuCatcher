93a6442685cb5844a27637bb18e2186b7f4a0766
hyee0715 <hyee0715@naver.com>
2023-09-13 15:08:48 +0900

    refactor: MemberEstimateResponse의 from 메서드 수정 (GS-58)

--- src/main/java/com/foo/gosucatcher/domain/estimate/application/dto/response/MemberEstimateResponse.java
+++ src/main/java/com/foo/gosucatcher/domain/estimate/application/dto/response/MemberEstimateResponse.java
@@ -1,8 +1,11 @@
 package com.foo.gosucatcher.domain.estimate.application.dto.response;
 
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
 import com.foo.gosucatcher.domain.estimate.domain.MemberEstimate;
+import com.foo.gosucatcher.domain.expert.domain.Expert;
 
 public record MemberEstimateResponse(
 	Long id,
@@ -20,8 +23,14 @@ public record MemberEstimateResponse(
 			return new MemberEstimateResponse(null, null, null, null, null, null, null);
 		}
 
+		Long expertIdFromMemberEstimate = null;
+
+		if (memberEstimate.getExpert() != null) {
+			expertIdFromMemberEstimate = memberEstimate.getExpert().getId();
+		}
+
 		return new MemberEstimateResponse(memberEstimate.getId(),
-			memberEstimate.getMember().getId(), memberEstimate.getExpert() == null ? null : memberEstimate.getExpert().getId(),
+			memberEstimate.getMember().getId(), expertIdFromMemberEstimate,
 			memberEstimate.getSubItem().getId(),
 			memberEstimate.getLocation(), memberEstimate.getPreferredStartDate(),
 			memberEstimate.getDetailedDescription());
