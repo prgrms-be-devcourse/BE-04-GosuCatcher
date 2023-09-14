cbf85960061082f491e090217299c99354e4a5f4
SW-H <hsw08@naver.com>
2023-09-14 16:40:55 +0900

    feat: 이미지 I/O 관련 인터페이스

--- src/main/java/com/foo/gosucatcher/domain/image/application/dto/response/ImageUploadResponse.java
+++ src/main/java/com/foo/gosucatcher/domain/image/application/dto/response/ImageUploadResponse.java
@@ -1,7 +1,9 @@
 package com.foo.gosucatcher.domain.image.application.dto.response;
 
+import java.util.List;
+
 public record ImageUploadResponse(
-	Long id,
-	String filename
+
+	List<String> filenames
 ) {
 }
