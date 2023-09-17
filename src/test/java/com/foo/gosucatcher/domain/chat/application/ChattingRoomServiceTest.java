7e7976a25bf5fcfa08db812d7e1963d78fd3a3da
hyee0715 <hyee0715@naver.com>
2023-09-17 17:43:30 +0900

    test: ChattingRoom 관련 테스트 코드 작성 (GS-88)

--- src/test/java/com/foo/gosucatcher/domain/chat/application/ChattingRoomServiceTest.java
+++ src/test/java/com/foo/gosucatcher/domain/chat/application/ChattingRoomServiceTest.java
@@ -4,11 +4,17 @@ import com.foo.gosucatcher.domain.chat.application.dto.response.ChattingRoomResp
 import com.foo.gosucatcher.domain.chat.application.dto.response.ChattingRoomsResponse;
 import com.foo.gosucatcher.domain.chat.domain.ChattingRoom;
 import com.foo.gosucatcher.domain.chat.domain.ChattingRoomRepository;
+import com.foo.gosucatcher.domain.estimate.domain.ExpertEstimate;
+import com.foo.gosucatcher.domain.estimate.domain.ExpertEstimateRepository;
 import com.foo.gosucatcher.domain.estimate.domain.MemberEstimate;
 import com.foo.gosucatcher.domain.estimate.domain.MemberEstimateRepository;
+import com.foo.gosucatcher.domain.estimate.domain.Status;
+import com.foo.gosucatcher.domain.expert.domain.Expert;
 import com.foo.gosucatcher.domain.item.domain.MainItem;
 import com.foo.gosucatcher.domain.item.domain.SubItem;
 import com.foo.gosucatcher.domain.member.domain.Member;
+import com.foo.gosucatcher.domain.member.domain.MemberRepository;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -35,6 +41,12 @@ class ChattingRoomServiceTest {
     @Mock
     private ChattingRoomRepository chattingRoomRepository;
 
+    @Mock
+    private MemberRepository memberRepository;
+
+    @Mock
+    private ExpertEstimateRepository expertEstimateRepository;
+
     @InjectMocks
     private ChattingRoomService chattingRoomService;
 
@@ -42,6 +54,9 @@ class ChattingRoomServiceTest {
     private MainItem mainItem;
     private SubItem subItem;
     private MemberEstimate memberEstimate;
+    private Expert expert;
+    private ExpertEstimate expertEstimate;
+
 
     @BeforeEach
     void setUp() {
@@ -63,6 +78,24 @@ class ChattingRoomServiceTest {
                 .preferredStartDate(LocalDateTime.now().plusDays(3))
                 .detailedDescription("추가 내용")
                 .build();
+
+        expert = Expert.builder()
+            .location("서울시 강남구")
+            .rating(4.0)
+            .maxTravelDistance(10)
+            .reviewCount(5)
+            .storeName("업체명")
+            .description("추가 설명입니다")
+            .member(member)
+            .build();
+
+        expertEstimate = ExpertEstimate.builder()
+            .expert(expert)
+            .memberEstimate(memberEstimate)
+            .subItem(subItem)
+            .totalCost(10000)
+            .description("견적서입니다")
+            .build();
     }
 
     @DisplayName("채팅방 생성 테스트")
@@ -104,12 +137,12 @@ class ChattingRoomServiceTest {
         List<ChattingRoomResponse> chattingRoomResponses = chattingRoomResponse.chattingRoomsResponse();
 
         //then
-        assertThat(expectedChattingRooms.size()).isEqualTo(chattingRoomResponses.size());
+        assertThat(chattingRoomResponses).hasSize(2);
     }
 
     @DisplayName("회원 요청서에 해당하는 모든 채팅방 조회 테스트")
     @Test
-    void findAllByMemberEstimate() {
+    void findAllByMemberEstimateId() {
         //given
         Long memberEstimateId = 1L;
         ChattingRoom chattingRoom = ChattingRoom.builder()
@@ -131,7 +164,37 @@ class ChattingRoomServiceTest {
         List<ChattingRoomResponse> chattingRoomResponses = chattingRoomResponse.chattingRoomsResponse();
 
         //then
-        assertThat(expectedChattingRooms.size()).isEqualTo(chattingRoomResponses.size());
+        assertThat(chattingRoomResponses).hasSize(2);
+    }
+
+    @DisplayName("회원 별 전체 채팅방 조회 테스트")
+    @Test
+    void findAllByMemberId() {
+        //given
+        Long memberId = 1L;
+
+        ChattingRoom chattingRoom = ChattingRoom.builder()
+            .memberEstimate(memberEstimate)
+            .build();
+
+        ChattingRoom chattingRoom2 = ChattingRoom.builder()
+            .memberEstimate(memberEstimate)
+            .build();
+
+        when(memberRepository.findById(memberId)).thenReturn(Optional.of(member));
+
+        when(memberEstimateRepository.findAllByMember(any(Member.class))).thenReturn(List.of(memberEstimate));
+
+        List<ChattingRoom> expectedChattingRooms = List.of(chattingRoom, chattingRoom2);
+
+        when(chattingRoomRepository.findAllByMemberEstimate(memberEstimate)).thenReturn(expectedChattingRooms);
+
+        //when
+        ChattingRoomsResponse chattingRoomResponse = chattingRoomService.findAllByMemberId(memberId);
+        List<ChattingRoomResponse> chattingRoomResponses = chattingRoomResponse.chattingRoomsResponse();
+
+        //then
+        assertThat(chattingRoomResponses).hasSize(2);
     }
 
     @DisplayName("id로 채팅방 조회 테스트")
@@ -151,6 +214,100 @@ class ChattingRoomServiceTest {
         //then
         assertThat(chattingRoomResponse.memberEstimateResponse().detailedDescription()).isEqualTo(memberEstimate.getDetailedDescription());
         assertThat(chattingRoomResponse.memberEstimateResponse().location()).isEqualTo(memberEstimate.getLocation());
+    }
+
+    @DisplayName("고수 별 전체 채팅방 조회 테스트")
+    @Test
+    void findAllByExpertId() {
+        //given
+        Long expertId = 1L;
+
+        ChattingRoom chattingRoom = ChattingRoom.builder()
+            .memberEstimate(memberEstimate)
+            .build();
+
+        ChattingRoom chattingRoom2 = ChattingRoom.builder()
+            .memberEstimate(memberEstimate)
+            .build();
+
+        ExpertEstimate expertEstimate2 = ExpertEstimate.builder()
+            .expert(expert)
+            .memberEstimate(memberEstimate)
+            .subItem(subItem)
+            .totalCost(10000)
+            .description("견적서입니다2")
+            .build();
+
+        when(expertEstimateRepository.findAllByExpertIdAndMemberEstimateIsNotNull(expertId)).thenReturn(List.of(expertEstimate, expertEstimate2));
+
+        when(chattingRoomRepository.findAllByMemberEstimate(memberEstimate)).thenReturn(List.of(chattingRoom, chattingRoom2));
+
+        //when
+        ChattingRoomsResponse chattingRoomResponse = chattingRoomService.findAllByExpertId(expertId);
+        List<ChattingRoomResponse> chattingRoomResponses = chattingRoomResponse.chattingRoomsResponse();
+
+        //then
+        assertThat(chattingRoomResponses).hasSize(4);
+    }
+
+    @DisplayName("고수의 일반 견적 관련 채팅방 목록 조회 테스트")
+    @Test
+    void findAllOfNormalByExpertId() {
+        //given
+        Long expertId = 1L;
+
+        ChattingRoom chattingRoom = ChattingRoom.builder()
+            .memberEstimate(memberEstimate)
+            .build();
+
+        ChattingRoom chattingRoom2 = ChattingRoom.builder()
+            .memberEstimate(memberEstimate)
+            .build();
+
+        when(memberEstimateRepository.findAllByExpertId(expertId)).thenReturn(List.of(memberEstimate));
+        when(chattingRoomRepository.findAllByMemberEstimate(memberEstimate)).thenReturn(List.of(chattingRoom, chattingRoom2));
+
+        ChattingRoomsResponse chattingRoomResponse = chattingRoomService.findAllOfNormalByExpertId(expertId);
+        List<ChattingRoomResponse> chattingRoomResponses = chattingRoomResponse.chattingRoomsResponse();
+
+        //then
+        assertThat(chattingRoomResponses).hasSize(2);
+    }
+
+    @DisplayName("고수의 바로 견적 관련 채팅방 목록 조회 테스트")
+    @Test
+    void findAllOfAutoByExpertId() {
+        //given
+        Long expertId = 1L;
+
+        memberEstimate.updateStatus(Status.PROCEEDING);
+
+        ChattingRoom chattingRoom = ChattingRoom.builder()
+            .memberEstimate(memberEstimate)
+            .build();
+
+        ChattingRoom chattingRoom2 = ChattingRoom.builder()
+            .memberEstimate(memberEstimate)
+            .build();
+
+        ExpertEstimate expertEstimate2 = ExpertEstimate.builder()
+            .expert(null)
+            .memberEstimate(memberEstimate)
+            .subItem(subItem)
+            .totalCost(10000)
+            .description("견적서입니다2")
+            .build();
+
+        when(expertEstimateRepository.findAllByExpertIdAndMemberEstimateIsNotNull(expertId)).thenReturn(List.of(expertEstimate, expertEstimate2));
+        when(chattingRoomRepository.findAllByMemberEstimate(memberEstimate)).thenReturn(List.of(chattingRoom, chattingRoom2));
+
+        //when
+        ChattingRoomsResponse chattingRoomResponse = chattingRoomService.findAllOfAutoByExpertId(expertId);
+        List<ChattingRoomResponse> chattingRoomResponses = chattingRoomResponse.chattingRoomsResponse();
+
+        //then
+        assertThat(chattingRoomResponses).hasSize(4);
+
     }
 
     @DisplayName("채팅방 삭제 테스트")
