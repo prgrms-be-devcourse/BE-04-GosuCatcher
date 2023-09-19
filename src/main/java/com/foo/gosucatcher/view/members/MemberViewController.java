2cd399590037e7aeaf53e6aba4506b0c603271eb
SW-H <hsw08@naver.com>
2023-09-19 17:15:06 +0900

    feat: 마이페이지 화면

---
+++ src/main/java/com/foo/gosucatcher/view/members/MemberViewController.java
@@ -0,0 +1,18 @@
+package com.foo.gosucatcher.view.members;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/gosu-catcher")
+public class MemberViewController {
+
+	@GetMapping("/members/my-page")
+	public String myPage() {
+		return "members/my-page";
+	}
+}
