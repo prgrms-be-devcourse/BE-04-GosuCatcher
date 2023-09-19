942d2faa750a47e903ca666e9ea5c33223f064a1
Hongseob Lee <sjun2918@naver.com>
2023-09-19 13:51:35 +0900

    feat: 메인 화면 구성 (GS-93)

---
+++ src/main/java/com/foo/gosucatcher/view/item/ItemController.java
@@ -0,0 +1,34 @@
+package com.foo.gosucatcher.view.item;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.foo.gosucatcher.domain.item.application.MainItemService;
+import com.foo.gosucatcher.domain.item.application.SubItemService;
+import com.foo.gosucatcher.domain.item.application.dto.response.sub.SubItemsSliceResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/gosu-catcher")
+public class ItemController {
+
+	private final MainItemService mainItemService;
+	private final SubItemService subItemService;
+
+	@GetMapping("/items")
+	public String items(@RequestParam("mainItemName") String mainItemName, Model model, @PageableDefault(page = 0, size = 10) Pageable pageable) {
+		SubItemsSliceResponse subItems = subItemService.findAllByMainItemName(mainItemName, pageable);
+
+		model.addAttribute("subItems", subItems);
+		model.addAttribute("mainItemName", mainItemName);
+
+		return "items/items";
+	}
+}
