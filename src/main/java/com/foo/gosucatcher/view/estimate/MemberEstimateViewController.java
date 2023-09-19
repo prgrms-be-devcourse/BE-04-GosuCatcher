76d79e1248f291b9884833d320d5bc3285b8ca5d
hyee0715 <hyee0715@naver.com>
2023-09-19 23:11:36 +0900

    feat: 바로 견적 요청서 화면 구성 (GS-110)

--- src/main/java/com/foo/gosucatcher/view/estimate/MemberEstimateViewController.java
+++ src/main/java/com/foo/gosucatcher/view/estimate/MemberEstimateViewController.java
@@ -5,17 +5,26 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/gosu-catcher/member-estimates")
+@RequestMapping("/gosu-catcher/hire")
 public class MemberEstimateViewController {
 
-	@GetMapping("/{memberId}/{expertId}/{subItemId}")
-	public String create(@PathVariable Long memberId, @PathVariable Long expertId, @PathVariable Long subItemId, Model model) {
+	@GetMapping("/normal/{subItemId}")
+	public String createNormal(@PathVariable Long subItemId, @RequestParam Long memberId, @RequestParam Long expertId, Model model) {
 		model.addAttribute("subItemId", subItemId);
 		model.addAttribute("memberId", memberId);
 		model.addAttribute("expertId", expertId);
 
-		return "estimate/member-estimate";
+		return "estimates/normal-member-estimate";
+	}
+
+	@GetMapping("/auto/{subItemId}")
+	public String createAuto(@PathVariable Long subItemId, @RequestParam Long memberId, Model model) {
+		model.addAttribute("subItemId", subItemId);
+		model.addAttribute("memberId", memberId);
+
+		return "estimates/auto-member-estimate";
 	}
 }
