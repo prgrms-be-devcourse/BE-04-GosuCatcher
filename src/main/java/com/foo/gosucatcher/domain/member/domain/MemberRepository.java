50193c94fb5236631943a1c418081a9ae09b6ac6
hyee0715 <hyee0715@naver.com>
2023-09-20 16:35:46 +0900

    refactor: MemberRepository의 existsByMemberIdAndRefreshToken 제거

--- src/main/java/com/foo/gosucatcher/domain/member/domain/MemberRepository.java
+++ src/main/java/com/foo/gosucatcher/domain/member/domain/MemberRepository.java
@@ -3,20 +3,10 @@ package com.foo.gosucatcher.domain.member.domain;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
 	Optional<Member> findByEmail(String email);
 
 	boolean existsByEmail(String email);
-
-	@Query("""
-		SELECT
-			CASE WHEN COUNT(m) > 0
-				THEN true ELSE false
-			END FROM Member m
-		WHERE m.id = :memberId AND m.refreshToken = :refreshToken
-		""")
-	boolean existsByMemberIdAndRefreshToken(Long memberId, String refreshToken);
 }
