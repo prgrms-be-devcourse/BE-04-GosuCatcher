0fbfb04b3c65964b9a4860c38953a4b1afa47a1a
hyee0715 <hyee0715@naver.com>
2023-09-21 23:12:37 +0900

    feat: 고수 응답 일반 견적서 화면으로 생성 기능 구현 (GS-121)

--- src/main/java/com/foo/gosucatcher/view/estimate/ExpertEstimateViewController.java
+++ src/main/java/com/foo/gosucatcher/view/estimate/ExpertEstimateViewController.java
@@ -1,8 +1,10 @@
 package com.foo.gosucatcher.view.estimate;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/gosu-catcher")
@@ -15,7 +17,8 @@ public class ExpertEstimateViewController {
 	}
 
 	@GetMapping("/normal-response")
-	public String createNormal() {
+	public String createNormal(@RequestParam Long memberEstimateId, Model model) {
+		model.addAttribute("memberEstimateId", memberEstimateId);
 
 		return "estimates/normal-expert-estimate";
 	}
