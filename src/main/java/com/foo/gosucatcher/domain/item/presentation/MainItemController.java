b7c98d6eab99c2ef332362d7d8dd5ea6901ed072
Hongseob Lee <sjun2918@naver.com>
2023-09-22 16:51:43 +0900

    docs: 메인 서비스 Swagger 적용

--- src/main/java/com/foo/gosucatcher/domain/item/presentation/MainItemController.java
+++ src/main/java/com/foo/gosucatcher/domain/item/presentation/MainItemController.java
@@ -17,8 +17,12 @@ import com.foo.gosucatcher.domain.item.application.dto.request.main.MainItemUpda
 import com.foo.gosucatcher.domain.item.application.dto.response.main.MainItemResponse;
 import com.foo.gosucatcher.domain.item.application.dto.response.main.MainItemsResponse;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
+@Tag(name = "메인 서비스 관련 Controller", description = "메인 서비스 등록,조회,수정,삭제")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/main-items")
@@ -26,13 +30,15 @@ public class MainItemController {
 
 	private final MainItemService mainItemService;
 
+	@Operation(summary = "메인 서비스 등록", description = "메인 서비스가 추가됩니다.")
 	@PostMapping
-	public ResponseEntity<MainItemResponse> create(@Validated @RequestBody MainItemCreateRequest request) {
+	public ResponseEntity<MainItemResponse> create(@Parameter(description = "서비스 등록 요청 정보", required = true) @Validated @RequestBody MainItemCreateRequest request) {
 		MainItemResponse mainItemResponse = mainItemService.create(request);
 
 		return ResponseEntity.ok(mainItemResponse);
 	}
 
+	@Operation(summary = "메인 서비스 전체 조회", description = "메인 서비스를 전체 조회할 수 있습니다.")
 	@GetMapping
 	public ResponseEntity<MainItemsResponse> findAll() {
 		MainItemsResponse mainItemsResponse = mainItemService.findAll();
@@ -40,22 +46,26 @@ public class MainItemController {
 		return ResponseEntity.ok(mainItemsResponse);
 	}
 
+	@Operation(summary = "메인 서비스를 ID로 조회", description = "메인 서비스를 ID로 조회 할 수 있습니다.")
 	@GetMapping("/{id}")
-	public ResponseEntity<MainItemResponse> findOne(@PathVariable Long id) {
+	public ResponseEntity<MainItemResponse> findOne(@Parameter(description = "찾을 서비스 ID", required = true, example = "1")
+													@PathVariable Long id) {
 		MainItemResponse mainItemResponse = mainItemService.findById(id);
 
 		return ResponseEntity.ok(mainItemResponse);
 	}
 
+	@Operation(summary = "메인 서비스 수정", description = "메인 서비스를 수정 할 수 있습니다.")
 	@PatchMapping("/{id}")
-	public ResponseEntity<MainItemResponse> update(@PathVariable Long id, @Validated @RequestBody MainItemUpdateRequest request) {
+	public ResponseEntity<MainItemResponse> update(@Parameter(description = "수정할 메인 서비스 ID", required = true, example = "1") @PathVariable Long id, @Parameter(description = "수정할 서비스 정보", required = true) @Validated @RequestBody MainItemUpdateRequest request) {
 		MainItemResponse mainItemResponse = mainItemService.update(id, request);
 
 		return ResponseEntity.ok(mainItemResponse);
 	}
 
+	@Operation(summary = "메인 서비스 삭제", description = "메인 서비스를 삭제 할 수 있습니다.")
 	@DeleteMapping("{id}")
-	public ResponseEntity<Void> delete(@PathVariable Long id) {
+	public ResponseEntity<Void> delete(@Parameter(description = "삭제할 메인 서비스 ID") @PathVariable Long id) {
 		mainItemService.delete(id);
 
 		return ResponseEntity.ok(null);
