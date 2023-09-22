785c0072b6044ee226e9a49e6762e8b0ad0f2439
SW-H <hsw08@naver.com>
2023-09-22 16:12:08 +0900

    test : swagger 설정

---
+++ src/main/java/com/foo/gosucatcher/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.foo.gosucatcher.config;
+
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import lombok.RequiredArgsConstructor;
+
+// SwaggerConfig.java
+@OpenAPIDefinition(
+	info = @Info(title = "Gosu Catcher API 명세서",
+		description = "에프팀의 고수 캐처 API 명세서",
+		version = "v1"))
+@RequiredArgsConstructor
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+	public GroupedOpenApi chatOpenApi() {
+		String[] paths = {"/api/v1/**"};
+
+		return GroupedOpenApi.builder()
+			.group("Gosu Catcher API 명세서")
+			.pathsToMatch(paths)
+			.build();
+	}
+}
